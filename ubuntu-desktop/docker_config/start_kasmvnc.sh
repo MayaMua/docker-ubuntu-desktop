ad76382ec04917b72fa8ff81b02b191b24e7970a
Jiawei Wu <wujuw@outlook.com>
2026-08-06 22:05:59 +0000

    update start_kasmvnc.sh

--- ubuntu-desktop/docker_config/start_kasmvnc.sh
+++ ubuntu-desktop/docker_config/start_kasmvnc.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-# set password for kasmvnc
+PASS="${PASSWORD}"
+if [ ${#PASS} -lt 6 ]; then
+    PASS="123456"
+fi
 if [ ! -f "/home/$USER/.vnc/passwd" ]; then
-    su $USER -c "echo -e \"$PASSWORD\n$PASSWORD\n\" | kasmvncpasswd -u $USER -o -w -r"
+    su $USER -c "echo -e \"$PASS\n$PASS\n\" | kasmvncpasswd -u $USER -o -w -r"
 fi
 rm -rf /tmp/.X1000-lock /tmp/.X11-unix/X1000
 # start kasmvnc
 if [ ! -z ${DISABLE_HTTPS+x} ]; then
-  su $USER -c "kasmvncserver :1000 -select-de xfce -interface 0.0.0.0 -websocketPort 4000 -sslOnly 0 -RectThreads $VNC_THREADS"
+  su $USER -c "kasmvncserver :1000 -select-de xfce -interface 0.0.0.0 -websocketPort 4000 -sslOnly 0 -RectThreads $VNC_THREADS -stunServer none -publicIP 127.0.0.1"
 else
-  su $USER -c "kasmvncserver :1000 -select-de xfce -interface 0.0.0.0 -websocketPort 4000 -cert $HTTPS_CERT -key $HTTPS_CERT_KEY -RectThreads $VNC_THREADS"
+  su $USER -c "kasmvncserver :1000 -select-de xfce -interface 0.0.0.0 -websocketPort 4000 -cert $HTTPS_CERT -key $HTTPS_CERT_KEY -RectThreads $VNC_THREADS -stunServer none -publicIP 127.0.0.1"
 fi
 su $USER -c "pulseaudio --start"
 tail -f /home/$USER/.vnc/*.log
